40da3a3b0670a3705564c596dfe60508aae9c451
Sebastian <sruanos@correo.url.edu.gt>
2025-05-23 08:13:32 -0600

    E02

--- src/main/java/ed/lab/E02Logger.java
+++ src/main/java/ed/lab/E02Logger.java
@@ -1,12 +1,20 @@
 package ed.lab;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class E02Logger {
+    private final Map<String, Integer> lastPrinted;
 
     public E02Logger() {
+        lastPrinted = new HashMap<>();
     }
 
     public boolean shouldPrintMessage(int timestamp, String message) {
+        if (!lastPrinted.containsKey(message) || timestamp - lastPrinted.get(message) >= 10) {
+            lastPrinted.put(message, timestamp);
+            return true;
+        }
         return false;
     }
-
 }
